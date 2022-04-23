2fe13f9af71dd6a48b2447b1a3fd504dae97f5f8
KamalDhillon <kkaur7887@gmail.com>
2022-04-23 15:18:38 -0700

    Codeadded

--- src/pullAndMergePractice/SimarCode.java
+++ src/pullAndMergePractice/SimarCode.java
@@ -1,5 +1,22 @@
 package pullAndMergePractice;
 
 public class SimarCode {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+       //checking length 
+		String str="Kamal";
+		int length=str.length();
+		System.out.println("length"+str.length());
+			
+		
+		int array[]= {50, 40, 20, 3, 4};
+		int length1=array.length;
+		System.out.println("lenghth1"+array.length);
+		
+		}
+
+	
 
 }
