4a4c33a8377487c2c63382d3a5b5a00f393f5fdc
Inderjit57 <singhinderjit57@yahoo.com>
2022-04-23 17:25:43 -0400

    format updated

--- src/pullAndMergePractice/InderCode.java
+++ src/pullAndMergePractice/InderCode.java
@@ -3,7 +3,7 @@ package pullAndMergePractice;
 public class InderCode {
 	public static void main(String[] args) {
 		// Write a program to find smallest and largest number in an array.
-//		Find the minimum number:
+		//Find the minimum number:
 
 		int[] num = { 10, 450, 12, 43, 56, 76, 34, 6, 5, 7, 1 };
 		int min = num[0];
@@ -15,7 +15,7 @@ public class InderCode {
 		}
 		System.out.println(min);
 
-//		Largest
+		// Find the Largest number
 		int[] num1 = { 10, 450, 12, 43, 56, 76, 34 };
 		int max = num1[0];
 
